d7f0226d571d08c03b9d7fd39d320a477a0d7551
Crenat <martsenyuk.yura@gmail.com>
2022-05-19 20:41:30 +0300

    initial commit

---
+++ Term Paper/LoginForm.cpp
@@ -0,0 +1,12 @@
+#include "LoginForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TermPaper::LoginForm form;
+	Application::Run(% form);
+}
